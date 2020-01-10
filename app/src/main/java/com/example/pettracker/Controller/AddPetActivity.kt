41bf60b01127594a02ab1960a28f7859281008c4
SaraSall <sara.sall@nentgroup.com>
2020-01-10 13:13:36 +0100

    String-se + AddPetActivity added

---
+++ app/src/main/java/com/example/pettracker/Controller/AddPetActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pettracker.Controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPetActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pet)
+    }
+}
